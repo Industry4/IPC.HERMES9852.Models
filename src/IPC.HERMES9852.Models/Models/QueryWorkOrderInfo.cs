fd20c83f601f7f68145a2da33716f3ef1f118dcb
4IR.UK <hello@industry4.uk>
2023-03-01 16:14:47 +0000

    #2

--- src/IPC.HERMES9852.Models/Models/QueryWorkOrderInfo.cs
+++ src/IPC.HERMES9852.Models/Models/QueryWorkOrderInfo.cs
@@ -4,24 +4,53 @@ namespace IPC.HERMES9852.Models
     [System.Xml.Serialization.XmlRoot(ElementName = "QueryWorkOrderInfo")]
     public class QueryWorkOrderInfo
     {
+        /// <summary>
+        /// Indicates the ID of QueryWorkOrder message.The ID must be unambiguous and e.g., can be a timestamp or a GUID.
+        /// </summary>
         [System.Runtime.Serialization.DataMember]
         [System.Xml.Serialization.XmlAttribute(AttributeName = "QueryId")]
         public string QueryId { get; set; }
 
+        /// <summary>
+        /// ID / name of this machine for identifying it in a Hermes enabled production line
+        /// </summary>
         [System.Runtime.Serialization.DataMember]
         [System.Xml.Serialization.XmlAttribute(AttributeName = "MachineId")]
         public string MachineId { get; set; }
 
+        /// <summary>
+        /// Barcode of a magazine, required to identify the magazine
+        /// </summary>
         [System.Runtime.Serialization.DataMember]
         [System.Xml.Serialization.XmlAttribute(AttributeName = "MagazineId")]
         public string MagazineId { get; set; }
 
+        /// <summary>
+        /// Indicates the slot in the magazine, enumerated from bottom to top, beginning with 1.
+        /// </summary>
         [System.Runtime.Serialization.DataMember]
         [System.Xml.Serialization.XmlAttribute(AttributeName = "SlotId")]
-        public string SlotId { get; set; }
+        public int? SlotId { get; set; }
 
+        /// <summary>
+        /// The barcode of the PCB.
+        /// </summary>
         [System.Runtime.Serialization.DataMember]
         [System.Xml.Serialization.XmlAttribute(AttributeName = "Barcode")]
         public string Barcode { get; set; }
+
+        /// <summary>
+        /// Identifies the work order for production of the PCB.
+        /// </summary>
+        [System.Runtime.Serialization.DataMember]
+        [System.Xml.Serialization.XmlAttribute(AttributeName = "WorkOrderId")]
+        public string WorkOrderId { get; set; }
+
+        /// <summary>
+        /// Identifies the Batch for production of the PCB within a splitted work order.  
+        /// </summary>
+        [System.Runtime.Serialization.DataMember]
+        [System.Xml.Serialization.XmlAttribute(AttributeName = "BatchId")]
+        public string BatchId { get; set; }
     }
 }
